a862164e7fd49291e740328003c4a2c51cb20536
Lorety-VL <vladimirking18@gmail.com>
2023-12-25 16:19:19 +0300

    completed

---
+++ src/StringSchema.js
@@ -0,0 +1,13 @@
+export default class StringSchema {
+    constructor (validators) {
+        this.validators = validators;
+    }
+    isValid(value) {
+        return this.validators.every((validator) => validator(value));
+    }
+    hasSpaces() {
+        // const validator = (value) => value.split('').filter((el) => el === ' ').length;
+        const validator = (value) => value.match(/\s/);
+        return new StringSchema([...this.validators, validator]);
+    }
+};
